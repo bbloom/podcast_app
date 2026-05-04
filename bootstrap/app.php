8c0046fa9e5ffc0b9fb5bb46da6e542029a5c4fb
Bob Bloom <bob.bloom@lasallesoftware.ca>
2026-05-04 14:51:49 -0400

    Add Sentry.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 
 // ============================================================================
@@ -70,5 +71,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions);
     })->create();    
